d1822c291d9ad56baaf52cbfee48f0f1c7fa168a
100jun001 <jwcom97@naver.com>
2025-03-10 18:32:12 +0900

    "Kiosk1b완성했습니다 어렵게 생각해서 오래걸렸네요"

--- src/lv1/Kiosk1b.java
+++ src/lv1/Kiosk1b.java
@@ -1,4 +1,54 @@
 package lv1;
 
+import java.util.Scanner;
+
 public class Kiosk1b {
+
+//    [ SHAKESHACK MENU ]
+//  1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+//  2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
+//  3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
+//  4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
+//  0. 종료      | 종료
+//  0 <- // 0을 입력
+    public static void main(String[] args) {
+        int a = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("0. 종료      | 종료");
+            System.out.println("0 <- // 0을 입력");
+
+            a = sc.nextInt();
+            sc.nextLine();
+
+            if (a == 1) {
+                System.out.println("1. ShackBurger 선택되었습니다.");
+
+            } else if (a == 2) {
+                System.out.println("2. SmokeShack 선택되었습니다.");
+
+            } else if (a == 3) {
+                System.out.println("3. Cheeseburger 선택되었습니다.");
+
+            } else if (a == 4) {
+                System.out.println("4. Hamburger 선택되었습니다.");
+
+            } else if (a > 4) {
+                System.out.println("해당하는 번호가 없습니다.");
+
+            } else if (a < 0) {
+                System.out.println("해당하는 번호가 없습니다.");
+
+            } else {
+                break;
+            }
+
+        }
+    }
 }
